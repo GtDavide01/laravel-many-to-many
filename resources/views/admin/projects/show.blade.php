e36a54df8cde9822041ad9360b87c1f1baafe9ee
GtDavide01 <davidefiorini01@gmail.com>
2023-01-17 15:50:30 +0100

    Add: technologies in show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,15 +2,34 @@
 
 @section('content')
     <div class="container">
+        {{-- Categoria  --}}
         <h4 class="text-center text-primary mt-3">
             {{ $project->type ? $project->type->name : 'Nessuna categoria' }}
         </h4>
+
+        {{-- Titolo progetto --}}
         <h1 class="text-center mt-3"> Nome Progetto : {{ $project->title }}</h1>
+
+        {{-- Informazioni base del progetto --}}
         <div class="d-flex justify-content-between mt-3">
             <h5> Data creazione : {{ $project->created_at }}</h5>
-            <p>{{ $project->slug }}</p>
+            <p class="fs-4">
+                Slug : {{ $project->slug }}</p>
         </div>
+        <h4>Descrizione del Progetto </h4>
         <p class="mt-3"> Descrizione Progetto : {{ $project->content }}</p>
+        {{-- Lista delle tecnologie usate  --}}
+        <div class="technologies">
+            <h4>Tecnologie usate : </h4>
+            @forelse ($project->technologies as $technology)
+                <span class="fw-semibold"> #{{ $technology->name }}</span>
+            @empty
+                <span>Nessun tag</span>
+            @endforelse
+        </div>
+
+        {{-- Immagine progetto --}}
+        <h4 class="mt-4">Foto del Progetto : </h4>
         @if ($project->image)
             <img src="{{ asset('storage/' . $project->image) }}" alt="">
         @else
@@ -18,6 +37,9 @@
                 <h3>Immagine non disponibile</h3>
             </div>
         @endif
+
+
+        {{-- Card del progetto --}}
         <div class="card mt-4" style="width: 18rem;">
             @if ($project->image)
                 <img src="{{ asset('storage/' . $project->image) }}" alt="">
